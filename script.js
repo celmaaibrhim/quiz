e5ab1d2c8db493648fc05c9f45a5a1000622a5e0
celmaaibrhim <73022813+celmaaibrhim@users.noreply.github.com>
2021-03-20 20:37:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const clickedButton = document.getElementById(clicked_id)
 clickedButton.classList.add('btn-clicked')
 }
 
+
+
+// var score = 1;
+
+// var addPoints = () =>{
+// var scoreAdded = score++;
+// document.getElementById('ramla').innerHTML = scoreAdded
+// }
